caa1d159501cf9a4685e01a1bf5f2f83360a8927
LaishaChavez <laishayalvanychavez@gmail.com>
2026-07-08 20:38:59 -0700

    Actualizar main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
+namespace SpriteKind {
+    export const Ayudante = SpriteKind.create()
+}
+function establecerAyudante () {
+    Ayudante = sprites.create(assets.image`miImagen`, SpriteKind.Ayudante)
+    Ayudante.ay = 0
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Dash.vy == 0) {
         Dash.vy = -100
     }
 })
+let Ayudante: Sprite = null
 let Dash: Sprite = null
 scene.setBackgroundImage(assets.image`Ala`)
-Dash = sprites.create(assets.image`Demoniado`, SpriteKind.Player)
+Dash = sprites.create(assets.image`Cubo`, SpriteKind.Player)
 controller.moveSprite(Dash, 30, 0)
 Dash.setStayInScreen(true)
 Dash.setPosition(0, 0)
 Dash.ay = 100
 tiles.setCurrentTilemap(tilemap`nivel3`)
+establecerAyudante()
+game.onUpdate(function () {
+    Ayudante.setPosition(Ayudante.x + (Dash.x - 20 - Ayudante.x) / 5, Ayudante.y + (Dash.y - 20 - Ayudante.y) / 5)
+})
